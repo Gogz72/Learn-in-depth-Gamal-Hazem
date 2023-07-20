82e65e56073467af085a8a63d2589fc9acc67548
Gogz72 <gamalhazemmohmed@gmail.com>
2023-07-20 03:09:43 +0300

    Unit 2 lecture 3 assignment is uploaded

---
+++ Assignments/Unit_2/Lecture_3/Assignment_1_HW_2/EX4.c
@@ -0,0 +1,41 @@
+/*
+ * EX3.c
+ *
+ *  Created on: 19 Jul 2023
+ *      Author: Gamal Hazem
+ */
+
+//#include "stdio.h"
+//
+//void main()
+//{
+//	float x;
+//	while(1)
+//	{
+//
+//
+//		printf ("Enter a number: ");
+//
+//		fflush(stdin); fflush(stdout);
+//
+//		scanf ("%f" , &x );
+//
+//		fflush(stdin); fflush(stdout);
+//
+//		if (x>0.0)
+//			printf("%f is positive\r\n" , x);
+//
+//		else if (x<0.0)
+//			printf("%f is negative\r\n" , x);
+//
+//		else
+//			printf("You entered zero\r\n");
+//
+//		fflush(stdin); fflush(stdout);
+//	}
+//
+//
+//
+//
+//}
+
---
+++ Assignments/Unit_2/Lecture_3/Assignment_1_HW_2/EX7.c
@@ -0,0 +1,46 @@
+/*
+ * EX7.c
+ *
+ *  Created on: 20 Jul 2023
+ *      Author: Gamal Hazem
+ */
+
+
+//#include "stdio.h"
+//
+//void main()
+//{
+//	int x , i;
+//	int factorial = 1;
+//	while(1)
+//	{
+//
+//		printf ("Enter an integer: ");
+//
+//		fflush(stdin); fflush(stdout);
+//
+//		scanf ("%d" , &x );
+//
+//		fflush(stdin); fflush(stdout);
+//
+//		if (x>0)
+//		{
+//			for (i=1 ; i<=x ; i++)
+//				factorial*=i;
+//
+//			printf("Factorial = %d\r\n" , factorial);
+//		}
+//
+//
+//		else if (x<0)
+//			printf("ERROR!!! Factorial of negative number doesn't exist.\r\n");
+//
+//		else
+//			printf("Factorial = 1\r\n");
+//
+//
+//
+//		fflush(stdin); fflush(stdout);
+//
+//	}
+//}
