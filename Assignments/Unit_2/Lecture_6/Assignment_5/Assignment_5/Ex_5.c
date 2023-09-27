698af33ab77d309e5dc0dc45576e2374b163db3f
Gogz72 <gamalhazemmohmed@gmail.com>
2023-09-27 11:07:42 +0300

    lecture_6 assignment

---
+++ Assignments/Unit_2/Lecture_6/Assignment_5/Assignment_5/Ex_5.c
@@ -0,0 +1,28 @@
+/////*
+//// * EX_5.c
+//// *
+//// *  Created on: 26 Sept 2023
+//// *      Author: Gamal Hazem
+//// */
+////
+//
+//#include <stdio.h>
+//
+//#define PI 3.14159
+//#define AREA(r) (PI * (r) * (r))
+//
+//int main() {
+//    double radius;
+//
+//    printf("Enter the radius: ");
+//    fflush(stdin);fflush(stdout);
+//
+//    scanf("%lf", &radius);
+//    fflush(stdin);fflush(stdout);
+//
+//    double area = AREA(radius);
+//
+//    printf("Area=%.2lf\n", area);
+//
+//    return 0;
+//}
